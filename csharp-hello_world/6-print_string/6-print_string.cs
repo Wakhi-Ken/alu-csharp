331ea91e0f0e9627369a545e856c82a3c393cac3
Wakhi-Ken <w.dlamini@alustudent.com>
2025-09-09 17:07:36 +0200

    3times n 9

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "Holberton School";
+        Console.WriteLine(str + str + str,"\nHolberton");
+        }
+}
