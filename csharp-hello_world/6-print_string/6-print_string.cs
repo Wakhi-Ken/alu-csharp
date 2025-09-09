ec5a98ca27f95c56ed0a552fc08e44b7e0460bdf
Wakhi-Ken <w.dlamini@alustudent.com>
2025-09-09 17:11:18 +0200

    fix

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -4,6 +4,7 @@ class Program
     static void Main(string[] args)
     {
         string str = "Holberton School";
-        Console.WriteLine(str + str + str,"\nHolberton");
-        }
+        Console.WriteLine(str + str + str);
+        Console.WriteLine(str.Substring(0, 9));
+    }
 }
