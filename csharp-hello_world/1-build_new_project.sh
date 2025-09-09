698b445046adb73dc4a8b12719d07a1bdb96ebad
Wakhi-Ken <w.dlamini@alustudent.com>
2025-09-09 19:55:22 +0200

    num1

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 # Script to initialize and build a new C# project
-
-# Folder name
 DIR="1-new_project"
 
-# Remove the folder if it already exists
 if [ -d "$DIR" ]; then
   rm -rf "$DIR"
 fi
-
-# Create a new console projects
 dotnet new console -o "$DIR"
 
-# Build the project
 dotnet build "$DIR"
