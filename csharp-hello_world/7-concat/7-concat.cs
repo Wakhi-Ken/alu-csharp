685b0bb02b899bf94252ac8d6ab820878d2869ec
Wakhi-Ken <w.dlamini@alustudent.com>
2025-09-12 10:06:29 +0200

    concrte

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+
 class Program
 {
     static void Main(string[] args)
     {
         string str1 = "Holberton";
         string str2 = "School";
-        Console.WriteLine("Welcome to {0}!", $"{str1} {str2}");
-	}
+        string fullStr = str1 + " " + str2;
+        Console.WriteLine("Welcome to {0}!", fullStr);
+    }
 }
