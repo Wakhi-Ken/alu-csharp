eb00e97603577a82ee7ae1398054bdb0b2e84324
Wakhi-Ken <w.dlamini@alustudent.com>
2025-09-13 18:55:39 +0200

    fizzbuzz

--- csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.Write("Fizz ");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.Write("Buzz ");
+            }
+            else if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.Write("FizBuzz ");
+            }
+            else
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
