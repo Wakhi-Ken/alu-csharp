a2f1a6fee7f9d015ab346086088c029878b756ff
Wakhi-Ken <w.dlamini@alustudent.com>
2025-09-13 16:10:36 +0200

    print combs

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+         if (i > 0)
+         Console.Write(", ");
+         Console.Write($"{i:D2}");
+        }
+    }
+}
