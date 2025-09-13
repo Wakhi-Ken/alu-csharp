385bc73d6649b1a07f980c209171454368020b75
Wakhi-Ken <w.dlamini@alustudent.com>
2025-09-13 17:30:19 +0200

    comb2

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -6,7 +6,7 @@ class Program
     {
         for (int i = 0; i < 100; i++)
         {
-         if (i > 0)
+         if (i < 99)
          Console.Write(", ");
          Console.Write($"{i:D2}");
         }
--- csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = i + 1; j < 10; j++)
+            {
+                Console.Write($"{i}{j}");
+                if (i != 8 || j != 9)
+                {
+                    Console.Write(", ");
+                }
+            }
+        }
+        Console.WriteLine();
+    }
+}
