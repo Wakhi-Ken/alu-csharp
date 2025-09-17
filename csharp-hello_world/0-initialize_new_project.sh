7d88da40754322f6c0a3a4a533205e1ee0209fb3
Wakhi-Ken <w.dlamini@alustudent.com>
2025-09-17 10:51:56 +0200

    fix

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -7,6 +7,6 @@ DIR="0-new_project"
 # Create project folder isf it doesn't exist
 mkdir -p "$DIR"
 # Initialize a new C# console project
-dotnet new console
+dotnet new console -o "$DIR"
 # dotnet automatically runs restore
 echo "C# project initialized in $DIR"
