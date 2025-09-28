0a80d2e195ceba5b614fb1b6974a4dde2dbb115c
Wakhi-Ken <w.dlamini@gmail.com>
2025-09-28 11:14:42 +0200

    2by2

--- csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -4,11 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[,] array = new int[5, 5]; // 5x5 array, all elements default to 0
+        int[,] array = new int[5, 5];
+        array[2, 2] = 1;
 
-        array[2, 2] = 1; // Set the middle element to 1
-
-        // Print the array
         for (int i = 0; i < 5; i++)
         {
             for (int j = 0; j < 5; j++)
