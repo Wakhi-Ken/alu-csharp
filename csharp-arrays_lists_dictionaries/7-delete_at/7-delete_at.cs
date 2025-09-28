dfa6e296b92de0107a75a20365775e574f9289a2
Wakhi-Ken <w.dlamini@gmail.com>
2025-09-28 10:30:52 +0200

    delete n number keys

---
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index < 0 || index >= myList.Count)
+        {
+            Console.WriteLine("Index is out of range");
+            return myList;
+        }
+
+        List<int> newList = new List<int>();
+
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (i != index)
+                newList.Add(myList[i]);
+        }
+
+        // Clear the original list and copy back the elements
+        myList.Clear();
+        foreach (int number in newList)
+            myList.Add(number);
+
+        return myList;
+    }
+}
---
+++ csharp-arrays_lists_dictionaries/8-number_key/8-number_key.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int count = 0;
+
+        foreach (string key in myDict.Keys)
+        {
+            count++;
+        }
+
+        return count;
+    }
+}
