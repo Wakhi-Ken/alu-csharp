855a5986f25cd15f5dcf3182f2e94eddfb2d328f
Wakhi-Ken <w.dlamini@gmail.com>
2025-09-28 09:29:22 +0200

    replace elements

---
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Array
+{
+    public static void ReplaceElement(int[] array, int index, int newValue)
+    {
+        if (array == null)
+        {
+            Console.WriteLine("Array is null");
+            return;
+        }
+
+        if (index < 0 || index >= array.Length)
+        {
+            Console.WriteLine("Index out of range");
+            return;
+        }
+
+        array[index] = newValue;
+    }
+}
