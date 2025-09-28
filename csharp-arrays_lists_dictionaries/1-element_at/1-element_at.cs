feccb59bc06ff61f07c8cb01f197577fb4d8b932
Wakhi-Ken <w.dlamini@gmail.com>
2025-09-28 09:21:50 +0200

    element

---
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        if (array == null)
+        {
+            Console.WriteLine("Array is null");
+            return -1;
+        }
+
+        if (index < 0 || index >= array.Length)
+        {
+            Console.WriteLine("Index out of range");
+            return -1;
+        }
+
+        return array[index];
+    }
+}
