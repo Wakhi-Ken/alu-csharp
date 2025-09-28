92cb927d0eb206f2befe9b18c5cc4ccf0b83e03c
Wakhi-Ken <w.dlamini@gmail.com>
2025-09-28 09:37:14 +0200

    print in reverse

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Array
+{
+    public static void PrintReverse(int[] array)
+    {
+        if (array == null)
+        {
+            Console.WriteLine("Array is null");
+            return;
+        }
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Console.Write(array[i]);
+            if (i > 0)
+                Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
