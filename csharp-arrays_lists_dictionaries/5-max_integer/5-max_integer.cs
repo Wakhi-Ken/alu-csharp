6507b7092a30fa43cc75df72d7347f5aefcdc37c
Wakhi-Ken <w.dlamini@gmail.com>
2025-09-28 10:15:13 +0200

    max fix

--- csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -5,18 +5,18 @@ public class List
 {
     public static int MaxInteger(List<int> myList)
     {
-        if (myList == null || myList.Count == 0)
+        if (myList.Count == 0)
         {
-            Console.WriteLine("List is empty or null");
-            return 0; // or throw an exception based on requirements
+            Console.WriteLine("List is empty");
+            return -1;
         }
 
         int max = myList[0];
 
-        foreach (int num in myList)
+        foreach (int number in myList)
         {
-            if (num > max)
-                max = num;
+            if (number > max)
+                max = number;
         }
 
         return max;
