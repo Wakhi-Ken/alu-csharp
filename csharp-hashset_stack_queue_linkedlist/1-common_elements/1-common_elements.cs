37732af3a275471de4ad1b75242ac64143d2755d
Wakhi-Ken <w.dlamini@gmail.com>
2025-09-30 10:23:34 +0200

    common elements

--- csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        if (list1 == null || list2 == null) return new List<int>(); // null-safety
+
+        HashSet<int> set1 = new HashSet<int>(list1);
+        HashSet<int> commonElements = new HashSet<int>();
+
+        foreach (int number in list2)
+        {
+            if (set1.Contains(number))
+            {
+                commonElements.Add(number);
+            }
+        }
+
+        return new List<int>(commonElements);
+    }
+}   
