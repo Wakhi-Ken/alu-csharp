584d8bd08f443a90a03316bf9731002cd1b4466f
Wakhi-Ken <w.dlamini@gmail.com>
2025-10-01 11:01:09 +0200

    print int

--- csharp-hashset_stack_queue_linkedlist/5-print_linkedlis/5-print_linkedlis.cs
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlis/5-print_linkedlis.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> llist = new LinkedList<int>();
+
+        if (size < 0)
+            return llist;
+
+        for (int i = 0; i < size; i++)
+        {
+            llist.AddLast(i);
+            Console.WriteLine(i);
+        }
+
+        return llist;
+    }
+}
