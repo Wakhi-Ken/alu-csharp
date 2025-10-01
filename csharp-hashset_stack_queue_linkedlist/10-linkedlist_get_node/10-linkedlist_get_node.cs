cb6bb5068df03131c950c52718cf4df01cfe3cd5
Wakhi-Ken <w.dlamini@gmail.com>
2025-10-01 11:40:33 +0200

    10-

--- csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (n < 0)
+            return 0;
+
+        int index = 0;
+        foreach (int value in myLList)
+        {
+            if (index == n)
+                return value;
+            index++;
+        }
+
+        return 0; // node doesn't exist
+    }
+}
