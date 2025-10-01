33f2916627319bced2fdb4b2e13a6b19f971e4b7
Wakhi-Ken <w.dlamini@gmail.com>
2025-10-01 11:12:13 +0200

    add node

--- csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
+++ csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        return myLList.AddFirst(n);
+    }
+}
