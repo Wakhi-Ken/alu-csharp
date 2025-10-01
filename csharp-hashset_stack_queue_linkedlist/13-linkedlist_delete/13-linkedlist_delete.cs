442dabc9d6e3188ebc45f301b89ecd1c3fb5cf57
Wakhi-Ken <w.dlamini@gmail.com>
2025-10-01 11:49:29 +0200

    13-main

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        if (index < 0 || index >= myLList.Count)
+            return; // invalid index
+
+        LinkedListNode<int> current = myLList.First;
+        int i = 0;
+
+        while (current != null && i < index)
+        {
+            current = current.Next;
+            i++;
+        }
+
+        if (current != null)
+            myLList.Remove(current);
+    }
+}
