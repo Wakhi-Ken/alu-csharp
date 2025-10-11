fbb7a59194caf040d97994888edda42b1df38a89
Wakhi-Ken <w.dlamini@gmail.com>
2025-10-11 19:15:35 +0200

    enum + struct

--- csharp-structs_enums/1-dog/1-dog.cs
+++ csharp-structs_enums/1-dog/1-dog.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public enum Rating
+{
+    Poor,
+    Average,
+    Good,
+    VeryGood,
+    Excellent
+}
+
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
