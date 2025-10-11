c6a6facad538842095c6ef9ab2e9593e3f9825e1
Wakhi-Ken <w.dlamini@gmail.com>
2025-10-11 19:30:48 +0200

    fixes

--- csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class List
+public class List
 {
     public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
     {
@@ -11,7 +11,8 @@ class List
         {
             try
             {
-                newList.Add(list1[i] / list2[i]);
+                int result = list1[i] / list2[i];
+                newList.Add(result);
             }
             catch (DivideByZeroException)
             {
@@ -21,7 +22,8 @@ class List
             catch (ArgumentOutOfRangeException)
             {
                 Console.WriteLine("Out of range");
-                newList.Add(0);
+                // stop further processing — don’t add anything more
+                break;
             }
         }
 
