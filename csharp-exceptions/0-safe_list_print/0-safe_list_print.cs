ff8e4a3b8ea5dbfee9247ee41fc84410fe7f68fb
Wakhi-Ken <w.dlamini@gmail.com>
2025-10-11 18:41:22 +0200

    catch an exception

--- csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count = 0;
+
+        try
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(myList[i]);
+                count++;
+            }
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+        //    Console.WriteLine("Caught an exception");
+        }
+
+        return count;
+    }
+}
