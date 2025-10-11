6ad035deddfbde3cc44ee99152b3f2d5832be350
Wakhi-Ken <w.dlamini@gmail.com>
2025-10-11 18:59:48 +0200

    exception with message

--- csharp-exceptions/4-throw_exception_msg/4-throw_exception_msg.cs
+++ csharp-exceptions/4-throw_exception_msg/4-throw_exception_msg.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class Except
+{
+    public static void ThrowMsg(string message)
+    {
+        List<int> myList = new List<int>();
+
+        try
+        {
+            Console.WriteLine(myList[0]);
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            throw new Exception(message);
+        }
+    }
+}
