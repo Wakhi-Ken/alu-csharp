49c168f6cde267f831ccaf1ca66032db82ec5865
Wakhi-Ken <w.dlamini@gmail.com>
2025-10-18 22:40:31 +0200

    health validation

--- csharp-classes/2-enemy/2-ememy.cs
+++ csharp-classes/2-enemy/2-ememy.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        public int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+    }
+}
