421a34180c7b40d30ab789b9290ad2d5383876d9
Wakhi-Ken <w.dlamini@gmail.com>
2025-11-12 19:38:17 +0200

    magnitude

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Vector2D
+{
+    public double X { get; }
+    public double Y { get; }
+
+    public Vector2D(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public double Magnitude()
+    {
+        return Math.Sqrt(X * X + Y * Y);
+    }
+}
