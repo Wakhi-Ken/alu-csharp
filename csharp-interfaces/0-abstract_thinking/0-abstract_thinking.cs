4e6cfbf87ed7c6d09fa497fcb40c434f8fa10a96
Wakhile Dlamini <w.dlamini@alustudent.com>
2026-02-24 10:05:27 +0200

    betty

--- csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{name} is a {GetType().Name}";
+    }
+}
