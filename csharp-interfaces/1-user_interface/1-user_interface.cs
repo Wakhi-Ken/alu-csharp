b12f4873612e7da2cbac3a4ae2f0893f7f85237c
Wakhile Dlamini <w.dlamini@alustudent.com>
2026-02-24 10:23:57 +0200

    base in 1-user interface

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public interface IInteractive
+{
+    void Interact();
+}
+
+public interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+public interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    // IBreakable property
+    public int durability { get; set; }
+
+    // ICollectable property
+    public bool isCollected { get; set; }
+
+    // IInteractive method
+    public void Interact()
+    {
+        // Not implemented
+    }
+
+    // IBreakable method
+    public void Break()
+    {
+        // Not implemented
+    }
+
+    // ICollectable method
+    public void Collect()
+    {
+        // Not implemented
+    }
+}
