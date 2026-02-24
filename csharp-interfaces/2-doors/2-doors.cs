9335edde805d0209381f70610ce388234fb90726
Wakhile Dlamini <w.dlamini@alustudent.com>
2026-02-24 10:31:29 +0200

    door locked

--- csharp-interfaces/2-doors/2-doors.cs
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Door : Base, IInteractive
+{
+    // Constructor with optional parameter
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+
+    // Implement Interact
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
