28486ec08266fa2d40b46a7be5d4e9d2935f631c
Wakhile Dlamini <w.dlamini@alustudent.com>
2026-03-03 14:49:13 +0200

    nix

--- csharp-hello_world/3-writeline/obj/Debug/net7.0/3-writeline.AssemblyInfo.cs
+++ csharp-hello_world/3-writeline/obj/Debug/net7.0/3-writeline.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3-writeline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d65177ace1cd3dd7e7588c880f75d9c48521a6df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e031ed61162b4d1ce4a45c97574f5fc271ef3379")]
 [assembly: System.Reflection.AssemblyProductAttribute("3-writeline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3-writeline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
